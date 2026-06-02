ee80a7ce09140e3988941aed978ce4e5d1a02aaf
Shivam Balikondwar <shivambalikondwar101@gmail.com>
2026-06-02 01:15:09 -0400

    Create 703kthlargestelement.py

---
+++ 703kthlargestelement.py
@@ -0,0 +1,56 @@
+import heapq
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.heap = sorted(nums)
+        self.k = k
+        idx = len(nums) - 1
+        while idx > 0 and idx > len(nums) - k:
+            idx-=1
+        
+        if idx < 0:
+            idx = 0
+        self.k_idx = idx
+
+
+    def floor_search(self, val):
+        lf = 0
+        rgt = len(self.heap) - 1
+        md = 0
+
+        while rgt - lf >= 0:
+            md = (lf + rgt) // 2
+            if val == self.heap[md]:
+                return md
+            elif val > self.heap[md]:
+                lf = md + 1
+            else:
+                rgt = md - 1
+        
+        # lf is always the upper bound, when we need to find an index for val >= val in array
+        # Find either the same value index or next index.
+        # Messed up the solution due to incorrect upper bound on binary search
+        return lf
+
+
+    def add(self, val: int) -> int:
+        # Get me index of one plus than the element index. 
+        idx = self.floor_search(val)
+
+        if len(self.heap) == 0:
+            self.heap.insert(0, val)
+            return self.heap[self.k_idx]
+        
+        if len(self.heap) < self.k:
+            self.heap.insert(idx, val)
+            return self.heap[self.k_idx]
+
+        if val >= self.heap[self.k_idx]:
+            self.heap.insert(idx, val)
+            if self.k_idx < len(self.heap) - 1:
+                self.k_idx+=1
+        else:
+            self.heap.insert(self.k_idx, val)
+            self.k_idx+=1
+        return self.heap[self.k_idx]
